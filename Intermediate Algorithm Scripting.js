0f38cbdc91aadc753d0faeffdf8f75f64d6c5188
Emmanuel Moshood <emmanuelmoshood571@gmail.com>
2023-06-10 07:39:56 -0400

    Sum All Numbers in a Range

--- Intermediate Algorithm Scripting.js
+++ Intermediate Algorithm Scripting.js
@@ -1,1 +1,19 @@
 /******************************************************Sum All Numbers in a Range */
+//sort array in increasing order
+//add numbers from start to end number inclusive
+//return sum
+
+const sumAll = (arr) => {
+    let sum;
+
+    const sortedArray = arr.sort((a,b) => a - b);
+
+    const fullArray = [];
+    for(let i = sortedArray[0]; i<=sortedArray[1]; i++){
+        fullArray.push(i);
+    }
+
+    return fullArray.reduce((acc, current) => acc + current);
+  }
+  
+  sumAll([1, 4]);
