a58840bad45e2153a8a41024a48acc1579844858
Emmanuel Moshood <emmanuelmoshood571@gmail.com>
2023-06-10 08:00:01 -0400

    symmetric difference

--- Intermediate Algorithm Scripting.js
+++ Intermediate Algorithm Scripting.js
@@ -14,6 +14,39 @@ const sumAll = (arr) => {
     }
 
     return fullArray.reduce((acc, current) => acc + current);
-  }
-  
-  sumAll([1, 4]);
+}
+//test
+sumAll([1, 4]);
+
+
+
+/****************************************************** return the symmetric difference of the two arrays */
+//take one array and compare against the other
+//use .indexOf(item) to find item that exist in on eand not the other
+
+const diffArray = (arr1, arr2) => {
+    const newArr = [];
+
+    // arr1.forEach(element => {
+    //     if(arr2.indexOf(element === -1)){
+    //         newArr.push(element)
+    //     }
+    // });
+
+    for(let i = 0; i<arr1.length; i++){
+        if(arr2.indexOf(arr1[i]) === -1){
+            newArr.push(arr1[i])
+        }
+    }
+    for(let i = 0; i<arr2.length; i++){
+        if(arr1.indexOf(arr2[i]) === -1){
+            newArr.push(arr2[i])
+        }
+    }
+
+
+    return newArr;
+}
+//test
+const test = diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
+console.log(test)
